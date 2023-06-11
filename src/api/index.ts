44f2ebc57bbfc27e7e066e708d431475e45074e5
choijingyeong <dldeo9987@gmail.com>
2023-06-11 18:59:35 +0900

    2트

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosResponse } from 'axios';
-import { IResponsePostList } from './types';
+import { IResponsePostList, TAG } from './types';
 
 const instance = axios.create({
   headers: {
@@ -14,7 +14,7 @@ export const getPostList = (): Promise<AxiosResponse<IResponsePostList>> => {
   return instance.get('/posts');
 };
 
-export const createPost = (title: string, contents: string, tag: string) => {
+export const createPost = (title: string, contents: string, tag: TAG) => {
   return instance.post('/posts', {
     title,
     contents,
@@ -22,21 +22,18 @@ export const createPost = (title: string, contents: string, tag: string) => {
   });
 };
 
-export const getPostById = (id: string | undefined) => {
+export const getPostById = (id: string) => {
   return instance.get(`/posts/${id}`);
 };
 
-export const updatePostById = (id: string, title: string, content: string, tag: TAG) => {
-  return (
-    instance.put(`posts/${id}`),
-    {
-      title,
-      content,
-      tag,
-    }
-  );
+export const updatePostById = (id: string, title: string, contents: string, tag: TAG) => {
+  return instance.put(`/posts/${id}`, {
+    title,
+    contents,
+    tag,
+  });
 };
 
-export const deletePostById = (id: string | undefined) => {
-  return instance.delete(`/posts/${id}`);
+export const deletePostById = (id: string) => {
+  instance.delete(`/posts/${id}`)
 };
